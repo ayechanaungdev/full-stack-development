5ed38e3dad1d8b31e64bc09a6548a8c53b9c3f7e
ayechanaungdev <ayechanaungdev@gmail.com>
2026-06-09 13:58:26 +0630

    Day 07: Bundle ChatModule and register in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { CarsModule } from './cars/cars.module';
 import { UsersModule } from './users/users.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-    imports: [PrismaModule, CarsModule, UsersModule, BookingsModule, AuthModule], // We will add other modules here later
+    imports: [PrismaModule, CarsModule, UsersModule, BookingsModule, AuthModule, ChatModule], // We will add other modules here later
     controllers: [AppController], // Routes go here
     providers: [AppService], // Business logic goes here
 })
